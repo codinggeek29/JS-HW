3a92aa22d587612641a26954ad92e19d0efe8035
codinggeek29 <166082786+codinggeek29@users.noreply.github.com>
2024-04-30 17:57:51 -0400

    Add files via upload

---
+++ HW_01_JS/index.js
@@ -0,0 +1,5 @@
+console.log("Welcome to this magic eight ball application.");
+const first_name = "Louisa";
+const last_name = "Kalisilira";
+let question = "How can I master JavaScript?";
+console.log("Hello may name is ${first_name} ${last_name} and my question is: ${question}");
